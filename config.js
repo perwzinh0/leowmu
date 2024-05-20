4e53127c5010b131a7edda8afb70b79150525042
Perwzinho <105089475+perwzinh0@users.noreply.github.com>
2024-05-20 01:14:14 -0300

    Update config.js
    Signed-off-by: Perwzinho <105089475+perwzinh0@users.noreply.github.com>

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-  TOKEN: "MTIxNzM1Mjc3ODcwMDAzMDA1Mg.GPv0m1.oPkkFuReDySKTbbIEHW7OkhZsgNUxL4Jaf4R_A",
-  ownerID: ["1161908168423641090", ""], 
+  TOKEN: "",
+  ownerID: ["", ""], 
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "00fbff",
